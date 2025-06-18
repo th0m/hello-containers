74e1a8b1084944dc811dfc1a068f6a9baeafa596
Thomas Lefebvre <tlefebvre@cloudflare.com>
2025-06-18 10:02:51 -0700

    feat: add logging in Go

--- container_src/main.go
+++ container_src/main.go
@@ -1,26 +1,31 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	"os"
+    "fmt"
+    "log"
+    "net/http"
+    "os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	message := os.Getenv("MESSAGE")
-	instanceId := os.Getenv("CLOUDFLARE_DEPLOYMENT_ID")
+    log.Printf("Received request: %s %s from %s", r.Method, r.URL.Path, r.RemoteAddr)
 
-	fmt.Fprintf(w, "Hi, I'm a container and this is my message: %s, and my instance ID is: %s", message, instanceId)
+    message := os.Getenv("MESSAGE")
+    instanceId := os.Getenv("CLOUDFLARE_DEPLOYMENT_ID")
+
+    log.Printf("Environment MESSAGE: %s", message)
+    log.Printf("Environment CLOUDFLARE_DEPLOYMENT_ID: %s", instanceId)
+
+    fmt.Fprintf(w, "Hi, I'm a container and this is my message: %s, and my instance ID is: %s", message, instanceId)
 }
 
 func errorHandler(w http.ResponseWriter, r *http.Request) {
-	panic("This is a panic")
+    panic("This is a panic")
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.HandleFunc("/container", handler)
-	http.HandleFunc("/error", errorHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+    http.HandleFunc("/", handler)
+    http.HandleFunc("/container", handler)
+    http.HandleFunc("/error", errorHandler)
+    log.Fatal(http.ListenAndServe(":8080", nil))
 }
